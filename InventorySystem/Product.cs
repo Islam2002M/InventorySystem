6f9bde2cccc369a459cb15165df20bfbe72a401c
Islam <islammsalam2002@gmail.com>
2024-09-14 14:11:18 +0300

    add a product.cs file to create a product class

---
+++ InventorySystem/Product.cs
@@ -0,0 +1,31 @@
+﻿namespace InventorySystem
+{
+    public class Product
+    {
+        private string name = string.Empty;
+        private int quntityInStock = 0;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Quantity
+        {
+            get { return quntityInStock; }
+            set { quntityInStock = value; }
+        }
+
+        public Product(int quntity):this(quntity,"")
+        {
+            
+        }
+        public Product(int quntity,string name)
+        {
+            Quantity = quntity;
+            Name = name;
+        }
+    }
+   
+}
--- InventorySystem/Program.cs
+++ InventorySystem/Program.cs
@@ -6,7 +6,6 @@ namespace InventorySystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
