36fca871cbab1ae1f8604674a4e7f4d26eabf3ff
Islam <islammsalam2002@gmail.com>
2024-09-14 17:53:01 +0300

    create the inventory class

---
+++ InventorySystem/Domain/inventory/Inventory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using InventorySystem.Domain.productMangment;
+
+namespace InventorySystem.Domain.inventory
+{
+    public class Inventory
+    {
+        private List <Product> products = new List<Product>();
+        
+    }
+    
+}
